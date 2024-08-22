b3cf80c685d13da23a5fa3fd4043325d6c145270
CodeMuscle <investportal99@gmail.com>
2024-08-22 19:10:21 +0530

    Completed Project

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,9 @@ import { WordPullUpDemo } from "@/components/demos/word-pull-up-demo";
 import { ScrollBasedVelocityDemo } from "@/components/demos/scroll-based-velocity-demo";
 import { BoxRevealDemo } from "@/components/demos/box-reveal-demo";
 import { AnimatedBeamMultipleOutputDemo } from "@/components/demos/animated-beam-demo";
+import { IconStarFilled } from "@tabler/icons-react";
+import { ShootingStarsAndStarsBackgroundDemo } from "@/components/demos/shooting-stars-demo";
+import LetsMakeThingsHappenSection from "@/components/ui/lets-make-things-happen";
 
 const services = [
   {
@@ -322,6 +325,71 @@ export default function Home() {
           </div>
         </main>
       </Element>
+
+      <section>
+        <main className="md:flex items-center justify-center space-y-6 md:space-y-0 md:gap-x-20 xl:w-4/5 2xl:w-[68%] mx-auto px-6 md:px-0">
+          <Image
+            src={"/logo/logo.webp"}
+            width={10000}
+            height={10000}
+            className=" md:w-1/3 rounded-md"
+            alt="image"
+          />
+          <div className="flex flex-col gap-y-5 md:w-1/2">
+            <h1 className="text-lg md:text-2xl ">
+              &quot;We&apos;ve been working with Bird for over 2 years and
+              they&apos;ve been amazing to work with. They&apos;ve helped us
+              grow our business and we couldn&apos;t be happier with the
+              results. &quot;
+            </h1>
+            <div className="flex items-center gap-x-1">
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+              <IconStarFilled className="text-4xl text-yellow-500" />
+            </div>
+
+            <span className="text-xl font-medium">
+              Jordan, Brisson <br />
+              CEO, Atlas Massage
+            </span>
+          </div>
+        </main>
+      </section>
+
+      <Element name="guarentees">
+        <ShootingStarsAndStarsBackgroundDemo />
+      </Element>
+
+      <section className="my-10 md:py-20 xl:w-4/5 2xl:w-[68%] md:mx-auto">
+        <LetsMakeThingsHappenSection />
+      </section>
+
+      <footer className="bg-[#fafafa] py-10  px-6 md:px-0 md:mx-auto border-t">
+        <div className="flex flex-col  justify-between gap-y-3 xl:w-4/5 2xl:w-[68%] mx-auto">
+          <h1 className="text-3xl md:text-5xl font-medium ">
+            <Image
+              src={"/logo/logo.webp"}
+              width={10000}
+              height={10000}
+              className="w-40"
+              alt="image"
+            />{" "}
+          </h1>
+          <p className="text-left  text-xl  text-gray-500">519-200-5000</p>
+          <p className="text-left  text-xl  text-gray-500">
+            admin@birdsoftware.ca
+          </p>
+        </div>
+
+        <div className="flex md:justify-center gap-x-4 mt-10">
+          © 2025 Bird. All Rights Reserved.
+          <Link href="/" className="text-blue-500">
+            Privacy Policy
+          </Link>
+        </div>
+      </footer>
     </div>
   );
 }
