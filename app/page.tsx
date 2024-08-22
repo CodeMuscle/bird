0a0b371e3202bce7bbca9513a6098e4785116f05
CodeMuscle <investportal99@gmail.com>
2024-08-22 11:24:29 +0530

    Brands Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import Image from 'next/image'
-import { Link as ScrollLink, Element } from 'react-scroll'
-import {AnimatedShinyTextDemo} from '@/components/demos/animated-shiny-text-demo';
-import { CoverDemo } from '@/components/demos/cover-demo';
+import Link from "next/link";
+import Image from "next/image";
+
+import { Link as ScrollLink, Element } from "react-scroll";
+import { AnimatedShinyTextDemo } from "@/components/demos/animated-shiny-text-demo";
+import { CoverDemo } from "@/components/demos/cover-demo";
+import { BoxReveal } from "@/components/magicui/box-reveal";
+import { PiCheckBold } from "react-icons/pi";
+import NumberTicker from "@/components/magicui/number-ticker";
+import { InfiniteMovingLogos } from "@/components/ui/infinite-moving-logos";
+import { WordPullUpDemo } from "@/components/demos/word-pull-up-demo";
 
 export default function Home() {
   return (
-    <div className="overflow-clip inset-0 -z-10 h-screen w-full bg-[#fafafa] bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+    <div className="overflow-clip inset-0 -z-10 h-full w-full bg-[#fafafa] bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
       <Element
         name="top"
         className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 xl:w-4/5 2xl:w-[68%] bg-white flex items-center justify-between py-6 px-4 md:px-8 mx-6"
@@ -67,14 +73,140 @@ export default function Home() {
         </div>
       </Element>
 
-      <main className="md:pb-10"> 
+      <main className="md:pb-10">
         <div className="md:px-10 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto mt-14">
           <AnimatedShinyTextDemo />
+          <h1>
+            <CoverDemo />
+          </h1>
+
+          <p className="md:text-center text-xl md:text-2xl my-6 md:my-10 md:w-4/5 mx-auto text-gray-500">
+            Schedule a call with us to discuss your project and get a quote in
+            minutes!
+          </p>
+
+          <div
+            className="
+                  flex 
+                    md:justify-center 
+                    items-center 
+                    gap-x-4
+                    "
+          >
+            <Link
+              href="/meeting"
+              className="py-3 
+              px-10
+              md:px-16
+        md:text-xl
+        hover:bg-[#abcbff] 
+        rounded-[6px]
+        border-2 
+        border-black 
+        dark:border-white 
+        bg-[#121212] 
+        text-white 
+        transition 
+        duration-200 
+        hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+            >
+              Book a Call
+            </Link>
+            <Link
+              href={"/showcase"}
+              className="
+                bg-white
+    py-3 
+    px-10
+    md:px-16
+        md:text-xl
+          border-4
+          border-black
+          rounded-[6px]
+          hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+            >
+              Showcase
+            </Link>
+          </div>
+
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-center text-left md:justify-items-center md:mx-auto mt-10 md:mt-16">
+            <BoxReveal boxColor={`#3b82f6`} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Design
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={`#3b82f6`} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Development
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={`#3b82f6`} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Marketing
+              </p>
+            </BoxReveal>
+            <BoxReveal boxColor={`#3b82f6`} duration={0.5}>
+              <p className="md:text-xl font-semibold flex gap-x-2 md:gap-x-4 items-center">
+                <PiCheckBold className="text-xl text-blue-500" />
+                Strategy
+              </p>
+            </BoxReveal>
+          </div>
+
+          <div className="md:flex items-center justify-between gap-y-4 my-10 gap-x-28 mx-auto">
+            <div className="md:w-2/5">
+              <h1 className="text-2xl font-medium text-gray-600 w-4/5">
+                Trusted by fast-moving brands worldwide
+              </h1>
+
+              <div className="flex my-6 gap-x-5 w-full">
+                <div className="">
+                  <h1 className="text-blue-500 text-3xl md:text-5xl">
+                    <NumberTicker value={10} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Happy Clients
+                    </p>
+                  </h1>
+                </div>
+
+                <div className="w-px bg-gray-300 self-stretch"></div>
+
+                <div className="flex-1 min-w-0">
+                  <h1 className="text-blue-500 text-3xl md:text-5xl whitespace-nowrap overflow-hidden">
+                    <NumberTicker value={25} /> +
+                    <p className="text-gray-500 text-sm md:text-md">
+                      Projects Completed
+                    </p>
+                  </h1>
+                </div>
+              </div>
+            </div>
+
+            <section className="overflow-hidden mt-10 md:w-4/5">
+              <InfiniteMovingLogos
+                speed="slow"
+                direction="left"
+                items={[
+                  { logo: "/logo/logo.webp", name: "Logo" },
+                  { logo: "/logo/logo.webp", name: "Logo" },
+                  { logo: "/logo/logo.webp", name: "Logo" },
+                ]}
+              />
+            </section>
+          </div>
         </div>
-        <h1>
-          <CoverDemo />
-        </h1>
       </main>
+
+      <Element name="services">
+        <div className="md:px-0 mx-6 xl:w-4/5 2xl:w-[68%] md:mx-auto">
+          <h1 className="text-3xl md:text-5xl md:text-center font-md">
+            <WordPullUpDemo />
+          </h1>
+        </div>
+      </Element>
     </div>
-  )
+  );
 }
